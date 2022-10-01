b091d67e363f966a1e420497cf4f455ef5d1abd4
Ethan Seither <ethan.seither@gmail.com>
2022-10-01 03:31:57 -0700

    fast with this rust engine
    why the heck doesn't it respect my desired max frames though

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,83 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::EventLoop;
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::window::WindowSettings;
+use std::{cmp, usize};
 use std::{thread, time};
 use std::io::{self, Write};
+use rand::distributions::{Distribution, Uniform};
+
+
 
 use::rayon::prelude::*;
 
-const GRID_SIZE: usize = 200;
+const GRID_SIZE: usize = 400;
 const NUM_ITER: i32 = 100;
-const DESIRED_FRAMES: u64 = 80;
+const DESIRED_FRAMES: u64 = 5;
 const FRAME_DELAY: time::Duration = time::Duration::from_millis(1000 / DESIRED_FRAMES);
 const PRINT_OUTPUT: bool = false;
+
+
+pub struct App {
+    gl: GlGraphics, // OpenGL drawing backend.
+    rotation: f64,  // Rotation for the square.
+    state: Vec<i8>,
+    rendered: bool,
+}
+impl App {
+    fn render(&mut self, args: &RenderArgs, status: &Vec<i8>) {
+        self.state = status.clone();
+        use graphics::*;
+
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 0.0];
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+
+        let square_size: f64 = (cmp::min(
+            (args.window_size[0] * 0.75) as usize,
+            (args.window_size[1] * 0.75) as usize) / GRID_SIZE) as f64;
+            self.gl.draw(args.viewport(), |c, gl| {
+                // Clear the screen.
+                clear(BLACK, gl);
+            });
+
+            for i in  0..(GRID_SIZE*GRID_SIZE) {
+                let (row, col) = to_coord(i);
+                let square = rectangle::square((square_size + 1.5) * col as f64,
+                (square_size + 1.5) * row as f64,
+                square_size);
+                // let rotation = self.rotation;
+                let (x, y) = (
+                    (square_size + 1.5) * col as f64,
+                    (square_size + 1.5) * row as f64
+                );
+
+                self.gl.draw(args.viewport(), |c, gl| {
+                    let transform = c
+                        .transform
+                        .trans(0.0, 0.0);
+
+                    let color = match self.state[i] {
+                        0 => BLACK,
+                        _ => WHITE,
+                    };
+                    rectangle(color, square, transform, gl);
+                });
+            }
+            self.rendered = true;
+    }
+
+    fn update(&mut self, args: &UpdateArgs, status: &Vec<i8>) {
+        self.state = status.clone();
+    }
+}
+
 
 enum Offset {
     Neg(usize),
@@ -27,13 +97,6 @@ const OFFSETS: [(Offset, Offset); 8] = [
     (Offset::Pos(1), Offset::Neg(1)), (Offset::Pos(1), Offset::Pos(0)), (Offset::Pos(1), Offset::Pos(1)),
 ];
 
-type Grid = [i8; GRID_SIZE*GRID_SIZE];
-
-fn get_at(g: Grid, i: usize, j: usize) -> i8 {
-    // row major
-    g[i*GRID_SIZE + j]
-}
-
 fn vec_at(g: &Vec<i8>, i: usize, j: usize) -> i8 {
     // row major
     g[i*GRID_SIZE + j]
@@ -41,11 +104,6 @@ fn vec_at(g: &Vec<i8>, i: usize, j: usize) -> i8 {
 fn set_vec(g: &mut Vec<i8>, i: usize, j: usize, val: i8){
     // row major
     g[i*GRID_SIZE + j] = val
-}
-
-fn set_at(g: &mut Grid, i: usize, j: usize, val: i8){
-    // row major
-    g[i*GRID_SIZE + j] = val;
 }
 
 // row, column
@@ -58,7 +116,14 @@ fn main() {
     //let mut life_even = [0; GRID_SIZE*GRID_SIZE];
 
     // simple blinker
-    let mut life_vec: Vec<i8> = vec![0; GRID_SIZE*GRID_SIZE];
+    let mut rng = rand::thread_rng();
+    let die: Uniform<i8>= Uniform::from(0..2);
+
+    let mut life_vec: Vec<i8> = (0..GRID_SIZE*GRID_SIZE).map( |_|
+        die.sample(&mut rng)
+    ).collect();
+
+
     set_vec(&mut life_vec, 1,1, 1);
     set_vec(&mut life_vec, 1,2, 1);
     set_vec(&mut life_vec, 1,3, 1);
@@ -67,71 +132,39 @@ fn main() {
 
     //let mut life_odd = [0; GRID_SIZE*GRID_SIZE];
 
+    let mut settings = EventSettings::new();
+    settings.set_max_fps(1);
+    settings.set_ups(0);
+
     let mut total_lag = time::Duration::from_millis(0);
 
-    for i in 0..NUM_ITER {
-        let now = time::Instant::now();
-        println!("\niteration {}", i);
-        if PRINT_OUTPUT {
-            print_vec(&life_vec);
-        }
-        life_vec = run_vec_step(&life_vec);
-        let passed = now.elapsed(); 
-        match FRAME_DELAY.checked_sub(passed) {
-            Some(d) => thread::sleep(d),
-            None => {
-                let delay = passed.checked_sub(FRAME_DELAY).expect("I'm gonna yartz");
-                // println!("oh wow yeah it's slow now boyo {:#?} ", delay);
-                total_lag = total_lag.saturating_add(delay);
-            },
+    // Change this to OpenGL::V2_1 if not working.
+    let opengl = OpenGL::V3_2;
+
+    // Create a Glutin window.
+    let mut window: Window = WindowSettings::new("spinning-square", [1920, 1200])
+        .graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    // Create a new game and run it.
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        rotation: 0.0,
+        state: life_vec.clone(),
+        rendered: false,
+    };
+
+    let mut events = Events::new(settings);
+    while let Some(e) = events.next(&mut window) {
+        if let Some(args) = e.render_args() {
+            life_vec = run_vec_step(&life_vec);
+            app.render(&args, &life_vec);
+            print!("getting rendered?");
         }
     }
-    println!("\ntotal lag from the slow iterations now {:#?} ", total_lag);
-}
 
-fn lives(src: Grid, i: usize, j: usize) -> bool {
-    let neigh_alive: i8 = OFFSETS.map( |(i_o, j_o)|
-        {
-            let i_coord = get_coord(i, i_o);
-            let j_coord = get_coord(j, j_o);
-            match (i_coord, j_coord) {
-                (Some(i_d), Some(j_d)) => {
-                    if i_d >= GRID_SIZE || j_d >= GRID_SIZE {
-                        0
-                    } else {
-                        get_at(src, i_d, j_d)
-                    }
-                },
-                (_, _) => 0,
-            }
-        }
-    ).into_iter().sum();
-
-    match get_at(src, i, j) == 1 {
-        true => {
-            // we alive now
-            neigh_alive == 2 || neigh_alive == 3 
-        },
-        false => {
-            neigh_alive == 3
-        },
-    }
-}
-
-fn run_step(from: Grid, to: &mut Grid) {
-    for i in 0..from.len(){
-        match from.get(i) {
-            Some(_) => {
-                let (i, j) = to_coord(i);
-                let res = match lives(from, i, j) {
-                    true => 1,
-                    false => 0,
-                };
-                set_at(to, i, j, res);
-            },
-            None => todo!(),
-        }
-    }
 }
 
 fn run_vec_step(from: &Vec<i8>) -> Vec<i8>  {
@@ -169,28 +202,6 @@ fn vec_lives(src: &Vec<i8>, idx:usize) -> i8 {
             (neigh_alive == 3).into()
         },
     }
-}
-
-fn print_arr(life_grid: Grid) {
-    let stdout = io::stdout();
-    let mut handle = io::BufWriter::new(stdout);
-
-    for i in 0..life_grid.len(){
-        if i % GRID_SIZE == 0 {
-            writeln!(handle, "").expect("toworklol");
-        } 
-        match life_grid.get(i) {
-            Some(life_row) => {
-                match life_row {
-                    0 => write!(handle, "\u{25FB} ").expect("towork"),
-                    1 => write!(handle, "\u{25FC} ").expect("justwork"),
-                    _ => panic!("lol impossible"),
-                }
-            },
-            None => panic!("my code never has bugs"),
-        }
-    }
-    handle.flush().expect("y can't u print though");
 }
 
 fn print_vec(life_grid: &Vec<i8>) {
