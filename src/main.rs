c00a605978173be66b41f52c3b92bb7e3994a13e
Ethan Seither <ethan.seither@gmail.com>
2022-09-30 23:05:06 -0700

    making this one array
    row major and will make the chunking up easier I think

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{thread, time};
 
-const GRID_SIZE: usize = 40;
-const NUM_ITER: i32 = 1000000;
+const GRID_SIZE: usize = 10;
+const NUM_ITER: i32 = 10;
 const DESIRED_FRAMES: u64 = 5;
 const FRAME_DELAY: time::Duration = time::Duration::from_millis(1000 / DESIRED_FRAMES);
 
@@ -23,23 +23,37 @@ const OFFSETS: [(Offset, Offset); 8] = [
     (Offset::Pos(1), Offset::Neg(1)), (Offset::Pos(1), Offset::Pos(0)), (Offset::Pos(1), Offset::Pos(1)),
 ];
 
-type Grid = [[i32; GRID_SIZE]; GRID_SIZE];
+type Grid = [i32; GRID_SIZE*GRID_SIZE];
+
+fn get_at(g: Grid, i: usize, j: usize) -> i32 {
+    // row major
+    g[i*GRID_SIZE + j]
+}
+fn set_at(g: &mut Grid, i: usize, j: usize, val: i32){
+    // row major
+    g[i*GRID_SIZE + j] = val;
+}
+
+// row, column
+fn to_coord(i: usize) -> (usize, usize) {
+    (i / GRID_SIZE, i % GRID_SIZE)
+}
 
 fn main() {
-    let mut life_even = [[0; GRID_SIZE] ; GRID_SIZE];
+    let mut life_even = [0; GRID_SIZE*GRID_SIZE];
 
     // simple blinker
-    life_even[1][1] = 1;
-    life_even[1][2] = 1;
-    life_even[1][3] = 1;
-    life_even[1][4] = 1;
-    life_even[1][5] = 1;
+    set_at(&mut life_even, 1,1, 1);
+    set_at(&mut life_even, 1,2, 1);
+    set_at(&mut life_even, 1,3, 1);
+    set_at(&mut life_even, 1,4, 1);
+    set_at(&mut life_even, 1,5, 1);
 
-    let mut life_odd = [[0; GRID_SIZE] ; GRID_SIZE];
+    let mut life_odd = [0; GRID_SIZE*GRID_SIZE];
 
     for i in 0..NUM_ITER {
         let now = time::Instant::now();
-        println!("iteration {}", i);
+        println!("\niteration {}", i);
         match i % 2 == 0 {
             true => {
                 print_arr(life_even);
@@ -50,7 +64,6 @@ fn main() {
                 run_step(life_odd, &mut life_even)
             }
         }
-        println!("\n");
         let passed = now.elapsed(); 
         match FRAME_DELAY.checked_sub(passed) {
             Some(d) => thread::sleep(d),
@@ -69,7 +82,7 @@ fn lives(src: Grid, i: usize, j: usize) -> bool {
                     if i_d >= GRID_SIZE || j_d >= GRID_SIZE {
                         0
                     } else {
-                        src[i_d][j_d]
+                        get_at(src, i_d, j_d)
                     }
                 },
                 (_, _) => 0,
@@ -77,7 +90,7 @@ fn lives(src: Grid, i: usize, j: usize) -> bool {
         }
     ).into_iter().sum();
 
-    match src[i][j] == 1 {
+    match get_at(src, i, j) == 1 {
         true => {
             // we alive now
             neigh_alive == 2 || neigh_alive == 3 
@@ -91,13 +104,13 @@ fn lives(src: Grid, i: usize, j: usize) -> bool {
 fn run_step(from: Grid, to: &mut Grid) {
     for i in 0..from.len(){
         match from.get(i) {
-            Some(from_row) => {
-                for j in 0..from_row.len(){
-                    to[i][j] = match lives(from, i, j) {
-                        true => 1,
-                        false => 0,
-                    };
-                }
+            Some(_) => {
+                let (i, j) = to_coord(i);
+                let res = match lives(from, i, j) {
+                    true => 1,
+                    false => 0,
+                };
+                set_at(to, i, j, res);
             },
             None => todo!(),
         }
@@ -106,17 +119,16 @@ fn run_step(from: Grid, to: &mut Grid) {
 
 fn print_arr(life_grid: Grid) {
     for i in 0..life_grid.len(){
+        if i % GRID_SIZE == 0 {
+            println!("");
+        } 
         match life_grid.get(i) {
             Some(life_row) => {
-                for j in 0..life_row.len(){
-                    match life_row.get(j) {
-                        Some(0) => print!("\u{25FB} "),
-                        Some(1) => print!("\u{25FC} "),
-                        Some(_) => panic!("lol impossible"),
-                        None => panic!(),
-                    }
+                match life_row {
+                    0 => print!("\u{25FB} "),
+                    1 => print!("\u{25FC} "),
+                    _ => panic!("lol impossible"),
                 }
-                println!("")
             },
             None => panic!("my code never has bugs"),
         }
